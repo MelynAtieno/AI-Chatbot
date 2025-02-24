d54e164554a91ce63f9ba66742fb6ab92261e0e8
Melyn Were <WereMA@cardiff.ac.uk>
2025-02-24 17:35:42 +0000

    update home file and add image

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("My first AI Chatbot")
+
+st.divider()
+
+st.write("Interact with different versions of the chatbot from the sidebar")
+
+st.image("./images/cat2.jpg")
+
+st.subheader("Built with love @MelynWere <3")
